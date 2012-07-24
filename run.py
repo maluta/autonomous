1e71db04b5a27ce4a9ed5835422b277d8e15fd05
Tiago Maluta <tiago.maluta+git@gmail.com>
2012-07-23 23:45:51 -0300

    Remove typo.

--- run.py
+++ run.py
@@ -7,9 +7,6 @@ address = [ "10.0.0.1", "10.0.0.1" ] # should be getG100ip()
 
 machines = [ ssh.Connection(address[i]) for i in range(len(address)) ]
 
-print g100s
-
-#s = ssh.Connection('10.0.0.1')
 for index,script in enumerate(scripts):
     print ":",script
     machines[index].put('%s' % script)
